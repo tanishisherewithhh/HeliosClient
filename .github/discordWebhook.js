c22f63acebb53e458890283c889715833f1304ac
tanishisherewith <120117618+tanishisherewithhh@users.noreply.github.com>
2024-03-11 22:59:38 +0530

    Small changes

--- .github/discordWebhook.js
+++ .github/discordWebhook.js
@@ -7,33 +7,35 @@ const repoName = process.env.GITHUB_REPOSITORY;
 const pushUser = process.env.PUSH_USER;
 const runID = process.env.RUN_ID;
 
-
 function sendBuildStatus(status) {
-  let title, description, color;
+  let title, description, color, buildOutput;
   if (status === 'success') {
     title = 'Build Successful';
-    description = 'The latest build of the project was successful.';
+    description = 'The build of the project was successful. Details...';
     color = 3066993; // green
+    buildOutput = `https://github.com/tanishisherewithhh/HeliosClient/actions/runs/${runID}`;
   } else if (status === 'failure') {
     title = 'Build Failed';
-    description = 'The latest build of the project failed.';
+    description = 'The latest build of the project failed. Details...';
     color = 15158332; // red
+    buildOutput = `null`;
   }
-
+  const shortCommit = commit.substring(0, 7);
   const data = {
     embeds: {
       title: title,
       description: description,
       color: color,
+      url: "https://github.com/HeliosMinecraft/HeliosClient",
      fields: [
         {
-          name: 'Push Actor',
+          name: 'Actor',
           value: pushUser,
           inline: true
         },
         {
           name: 'Commit',
-          value: `[Here](https://github.com/${repoName}/commit/${commit})`,
+          value: `[${shortCommit}](https://github.com/${repoName}/commit/${commit})`,
           inline: true
         },
         {
@@ -43,14 +45,21 @@ function sendBuildStatus(status) {
         },
         {
           name: 'Build Output',
-          value: `[Output](https://github.com/tanishisherewithhh/HeliosClient/actions/runs/${runID})`,
+          value: `[Build link](${buildOutput})`,
           inline: true
         }
-      ]
-    }
+      ],
+      footer: {
+        text: 'From Github Actions',
+      },
+      timestamp: new Date().toISOString()
+    },
+   avatar_url: "https://media.discordapp.net/attachments/1216707490570309744/1216798445684264990/icon.png?ex=6601b2dc&is=65ef3ddc&hm=6937eb0df5b51988369fd611ed5ee55c48ef2f906fba8caf572f7d0ea0a29c3f&=&format=webp&quality=lossless"
   };
 
   axios.post(webhookURL, {
+  avatar_url: data.avatar_url,
+  name: "HeliosClient",
   embeds: [data.embeds] // send the embeds as an array
 })
 .then(response => console.log(response.data))
