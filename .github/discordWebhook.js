81ec4744e879c099bf5774e25b5885df1362e817
tanishisherewith <120117618+tanishisherewithhh@users.noreply.github.com>
2024-03-11 20:39:22 +0530

    Update discordWebhook.js

--- .github/discordWebhook.js
+++ .github/discordWebhook.js
@@ -4,22 +4,34 @@ const webhookURL = process.env.DISCORD_WEBHOOK;
 const commit = process.env.GITHUB_SHA;
 const runNumber = process.env.GITHUB_RUN_NUMBER;
 const repoName = process.env.GITHUB_REPOSITORY;
+const pushUser = process.env.PUSH_USER;
 
-const data = {
-  embeds: 
-    {
-      title: 'Build Successful',
-      description: `The build of the project was successful. Here are some details...`,
-      color: 3066993,
-      fields: [
+function sendBuildStatus(status) {
+  let title, description, color;
+  if (status === 'success') {
+    title = 'Build Successful';
+    description = 'The latest build of the project was successful.';
+    color = 3066993; // green
+  } else if (status === 'failure') {
+    title = 'Build Failed';
+    description = 'The latest build of the project failed.';
+    color = 15158332; // red
+  }
+
+  const data = {
+    embeds: {
+      title: title,
+      description: description,
+      color: color,
+     fields: [
         {
-          name: 'Repository',
-          value: repoName,
+          name: 'Push Actor',
+          value: pushUser,
           inline: true
         },
         {
           name: 'Commit',
-          value: commit,
+          value: '[Here](https://github.com/${repoName}/commit/${commit})',
           inline: true
         },
         {
@@ -29,24 +41,20 @@ const data = {
         },
         {
           name: 'Build Output',
-          value: "https://github.com/tanishisherewithhh/HeliosClient/.github/workflows/gradle.yml",
+          value: '[Output](https://github.com/tanishisherewithhh/HeliosClient/.github/workflows/gradle.yml)',
           inline: true
         }
       ]
-    }
-};
- axios.patch(webhookUrl, {
-        name: 'HeliosClient',
-    }).then(() => {
-        console.log('Webhook name updated successfully');
-    }).catch((error) => {
-        console.error('Failed to update webhook name:', error);
-    });
+    }]
+  };
 
-axios.post(webhookURL, {
-  embeds: [data.embeds] // send the embeds as an array
-})
-.then(response => console.log(response.data))
-.catch((error) => {
-  console.error('Error:', error);
-});
+  axios.post(webhookURL, data)
+    .then(response => console.log(response.data))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+// Call the function with the build status
+const status = process.argv[2]; // get the status from the command line arguments
+sendBuildStatus(status); // 'success' or 'failure'
